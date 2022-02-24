d718f5181d025c7af82196df25ad60e2ee73bd4d
Vaishnavi Kadam <95268595+vaishnavikadam19@users.noreply.github.com>
2022-02-24 12:03:49 +0530

    Add project files.

---
+++ VAC_Interio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VAC_Interio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
