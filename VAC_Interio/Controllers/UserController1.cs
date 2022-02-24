6c43f58d76237116f4c4aed3fb8a90726cbc3c02
Arunvh <95465253+Arunvh@users.noreply.github.com>
2022-02-24 12:20:27 +0530

    Arun -1

---
+++ VAC_Interio/Controllers/UserController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VAC_Interio.Controllers
+{
+    public class UserController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
